864c6f136a1ddfe1a9d9dc3a012284c1a622f94d
Riany Mello <mello.riany@gmail.com>
2023-05-20 17:12:51 +0100

    :construction: Banner and Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,8 +5,19 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
-import logo from "../logo.png"
+import colorSharp from "../assets/img/color-sharp.png";
+import logo from "../logo.png";
+import iconReact from "../assets/img/react-icon.png";
+import iconJavascript from "../assets/img/javacript-icon.png";
+import iconGit from "../assets/img/git-icon.png";
+import iconGithub from "../assets/img/github-icon.png";
+import iconTypescript from "../assets/img/typescript-icon.png";
+import iconCss from "../assets/img/css-icon.png";
+import iconHtml from "../assets/img/html-icon.png";
+import iconFigma from "../assets/img/figma-icon.png";
+import iconMiro from "../assets/img/miro-icon.png";
+import iconTrello from "../assets/img/trello-icon.png";
+
 
 export const Skills = () => {
   const responsive = {
@@ -39,39 +50,43 @@ export const Skills = () => {
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                           <div className="item">
-                            <img src={logo} alt="Image" />
+                            <img src={iconReact} alt="Image" />
                             <h5>React.js</h5>
                           </div>
                           <div className="item">
-                          <img src={logo} alt="Image" />
+                          <img src={iconJavascript} alt="Image" />
                             <h5>Javacript</h5>
                           </div>
                           <div className="item">
-                          <img src={logo} alt="Image" />
-                          <h5>Git e Github</h5>
+                          <img src={iconGit} alt="Image" />
+                          <h5>Git</h5>
                           </div>
                           <div className="item">
-                          <img src={logo} alt="Image" />
+                          <img src={iconGithub} alt="Image" />
+                          <h5>Github</h5>
+                          </div>
+                          <div className="item">
+                          <img src={iconTypescript} alt="Image" />
                           <h5>Typescript</h5>
                           </div>
                           <div className="item">
-                          <img src={logo} alt="Image" />
+                          <img src={iconCss} alt="Image" />
                             <h5>CSS</h5>
                           </div>
                           <div className="item">
-                          <img src={logo} alt="Image" />
+                          <img src={iconHtml} alt="Image" />
                             <h5>HTML</h5>
                           </div>
                           <div className="item">
-                          <img src={logo} alt="Image" />
+                          <img src={iconFigma} alt="Image" />
                             <h5>Figma</h5>
                           </div>
                           <div className="item">
-                          <img src={logo} alt="Image" />
+                          <img src={iconTrello} alt="Image" />
                             <h5>Trello</h5>
                           </div>
                           <div className="item">
-                          <img src={logo} alt="Image" />
+                          <img src={iconMiro} alt="Image" />
                             <h5>Miro</h5>
                           </div>
                         </Carousel>
