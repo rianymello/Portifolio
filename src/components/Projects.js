f5486741bc534c5f0287582d32ffa68ece06abd3
Riany Mello <mello.riany@gmail.com>
2023-05-21 14:22:05 +0100

    :construction: Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,7 @@ import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const [activeTab, setActiveTab] = useState("first");
 
   const projectsTab1 = [
     {
@@ -27,7 +28,7 @@ export const Projects = () => {
       description: "Design & Development",
       imgUrl: projImg3,
     },
-    // Add more projects as needed
+  
   ];
 
   const projectsTab2 = [
@@ -45,8 +46,13 @@ export const Projects = () => {
       title: "Business Startup 6",
       description: "Design & Development",
       imgUrl: projImg3,
+    }, 
+    {
+      title: "Business Startup 6",
+      description: "Design & Development",
+      imgUrl: projImg3,
     },
-    // Add more projects as needed
+   
   ];
 
   const projectsTab3 = [
@@ -65,8 +71,15 @@ export const Projects = () => {
       description: "Design & Development",
       imgUrl: projImg3,
     },
-    // Add more projects as needed
+   
   ];
+
+  const tab1Text = "Esses são alguns dos projetos que eu venho desenvolvendo por sozinha.";
+  const tab2Text = "Esses são alguns dos projetos que desenvolvi durante meu curso de gestão e programção de sistemas informáticos.";
+  const tab3Text = "Esses são outros projetos também desenvolvidos por mim.";
+
+  const currentTabText = activeTab === "first" ? tab1Text : activeTab === "second" ? tab2Text : tab3Text;
+
 
   return (
     <section className="project" id="project">
@@ -77,20 +90,18 @@ export const Projects = () => {
               <div className={`animate__animated ${isVisible ? "animate__fadeIn" : ""}`}>
                 <h2>Projects</h2>
                 <p>
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                  when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                  {currentTabText}
                 </p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Tab.Container id="projects-tabs" activeKey={activeTab} onSelect={(tab) => setActiveTab(tab)}>
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      <Nav.Link eventKey="first">Projetos pessoais</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      <Nav.Link eventKey="second">Projetos escolares</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">Outros projetos</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
