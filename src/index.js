41303d807165a1a62342f288e8741ded78fb2a08
Riany Mello <mello.riany@gmail.com>
2023-05-23 10:00:09 +0100

    :boom: fix: -Modal, +img

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactModal from 'react-modal';
-import 'react-modal/dist/react-modal.css';
+import ReactModal from 'react-modal'
 ReactModal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
